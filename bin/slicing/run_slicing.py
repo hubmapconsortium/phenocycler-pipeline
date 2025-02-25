c38b22021a324cf526f3d5d2a9a629794f091d1f
Matt Ruffalo <matt.ruffalo@gmail.com>
2025-02-25 13:43:57 -0500

    Drop import of removed function

--- bin/slicing/run_slicing.py
+++ bin/slicing/run_slicing.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from typing import Tuple
 
 import tifffile as tif
-from modify_pipeline_config import modify_pipeline_config, save_modified_pipeline_config
+from modify_pipeline_config import modify_pipeline_config
 from slicer import slice_img
 
 filename_pattern = re.compile(r"^aligned_tissue_0_(?P<channel>\w+).tif$")
